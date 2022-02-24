d82edb6b2337fa3c719380b69019bb67ce6f1726
LBF1234 <92590948+LeviF1234@users.noreply.github.com>
2022-02-24 16:40:05 +0000

    e

---
+++ script.js
@@ -0,0 +1,18 @@
+const k = 'your mom';
+const i = 'is so fat';
+var d = 'that she sunk the Titanic';
+
+console.log(k, i, d)
+var d = 'that when she walks home from work her belly button gets home 15 minutes bafore her'
+console.log(
+  
+)
+console.log(k, i, d)
+console.log(
+  
+)
+console.log(k)
+console.log(
+  
+)
+console.log(k, i)
